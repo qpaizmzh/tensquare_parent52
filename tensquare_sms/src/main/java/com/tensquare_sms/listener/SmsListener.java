88a88907900ad71f2de1bfd1f1a3b7d76e1de59a
qpaizmzh <blusla@sina.com>
2019-06-16 10:23:30 +0800

    更新了JWS

--- tensquare_sms/src/main/java/com/tensquare_sms/listener/SmsListener.java
+++ tensquare_sms/src/main/java/com/tensquare_sms/listener/SmsListener.java
@@ -1,6 +1,7 @@
-package com.tensquare_sms.listener;
+package com.tensquare_sms.Listener;
 
-import com.tensquare_sms.utils.SmsUtil;
+import com.aliyuncs.exceptions.ClientException;
+import com.tensquare_sms.SmsUtils.SmsUtil;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +11,28 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-@RabbitListener(queues = "忘了是什么队列了")
+@RabbitListener(queues = "msgs")
 public class SmsListener {
 
     @Autowired
     private SmsUtil utils;
 
-    @Value("${aliyun.sms.template_code}")
-    private String template_code;
+    @Value(value = "${aliyun.sms.accessKeyId}")
+    private String accessKeyId;
 
-    @Value("${aliyun.sms.sign_name}")
-    private String sign_name;
-
+    @Value(value = "${aliyun.sms.accessKeySecret}")
+    private String accessKeySecret;
 
     @RabbitHandler
-    public void getMSg(Map<String, String> maps) {
-        System.out.println("手机号是：" + maps.get("mobile"));
-        System.out.println("验证码是：" + maps.get("code"));
+    public void getMsg(Map<String,String> maps){
+        System.out.println("mobile "+ maps.get("mobile"));
+        System.out.println("code "+ maps.get("random"));
+
+        try {
+            //短信内容要换成JSON串,这里的checkcode键是要给开通短信网站的模板使用的，那里的使用模板会有个${checkcode}的表达式存在，用来调用下面准备发送过去的JSON串的
+            utils.sendSms(maps.get("mobile"),accessKeyId,accessKeySecret,"{\"checkcode\":\""+maps.get("random")+"\"}");
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
     }
 }
